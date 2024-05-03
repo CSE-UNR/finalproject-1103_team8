753f277802df91ac4354ade77ddb8c37507c3300
Reina Antaredjo <rantaredjo@nevada.unr.edu>
2024-05-03 17:28:41 +0000

    images.c

---
+++ images.c
@@ -0,0 +1,37 @@
+// Authors:
+// Assignment: Final Project 
+// Date: 5/3/24
+
+#include <stdio.h>
+int main() {
+
+
+
+	return 0;
+}
+
+void cropImage(){
+
+	return ;
+}
+
+void brightenImage(){
+
+	return; 
+}
+
+void dimImage() {
+
+	return;
+}
+
+void saveImage() {
+
+	return;
+}
+
+void loadImage(){
+
+	return;
+}
+
