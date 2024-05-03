860a466feb7dea93c494f4a9f150cec3e9f3dcc1
1103-shahnazarian-milana <mshahnazarian@nevada.unr.edu>
2024-05-03 17:38:34 +0000

    new changes

--- images.c
+++ images.c
@@ -1,13 +1,27 @@
 // Authors:
 // Assignment: Final Project 
 // Date: 5/3/24
+ void menu();
+ void cropImage();
+ void brightenImage();
+ void dimImage();
+ void saveImage();
+ void loadImage();
+ 
+ 
 
 #include <stdio.h>
 int main() {
-
+ printf("Welcome to Erinstagram! \n");
 
 
 	return 0;
+}
+
+void menu(){
+
+
+
 }
 
 void cropImage(){
